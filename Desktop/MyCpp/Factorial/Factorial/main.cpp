30522fb80a979d53dd76741e5051c86c0eb7af79
Lisa Ding <palisade0401@gmail.com>
2014-10-11 03:47:06 +0800

    first commit

---
+++ Desktop/MyCpp/Factorial/Factorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Factorial
+//
+//  Created by lisading on 10/10/14.
+//  Copyright (c) 2014 lisading. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
